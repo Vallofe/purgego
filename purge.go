fa353ca7ac6d745741de4bc736a0fed15bf7688f
Vallofe <55863376+Vallofe@users.noreply.github.com>
2020-11-28 14:48:55 +0700

    Update purge.go

--- purge.go
+++ purge.go
@@ -2,7 +2,11 @@ package purge
 
 import (
     "os/exec"
+    "fmt"
 )
 
-func dualmode(client string,to string,stats string,app string){
-}
+func dualMode(gid string, authtoken string, app string, sub string, sub2 string){
+    data := fmt.Sprintf("node /root/dual.js gid=%s %s %s token=%s app=%s",gid,sub,sub2,authtoken,app)
+    data2 := strings.Fields(data)
+    command, _ := exec.Command(data2[0],data2[1:]...).Output()
+    fmt.Println(string(command))
